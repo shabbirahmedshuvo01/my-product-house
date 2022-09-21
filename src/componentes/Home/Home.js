11ec8dae0d0224fca72b54b2ec3e186c9c8af3af
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-09-22 00:44:35 +0600

    helmet implemnted

--- src/componentes/Home/Home.js
+++ src/componentes/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import PageTitle from '../Helmet/PageTitle';
 
 const Home = () => {
     return (
         <div>
+            <PageTitle title="Home"></PageTitle>
             <h1 className='text-2xl'>how are you</h1>
         </div>
     );
